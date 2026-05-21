850ddaa230e7ff55d26d15f6a66fcfe35c1eb29d
mdtabrezhossain <142249992+mdtabrezhossain@users.noreply.github.com>
2026-05-22 00:11:21 +0530

    added update and remove pothole endpoint

--- backend/src/controllers/pothole.controller.ts
+++ backend/src/controllers/pothole.controller.ts
@@ -11,9 +11,9 @@ export async function addPothole(request: Request, response: Response) {
         direction,
         image_link,
     } = request.body.pothole_details;
-    const { user_id } = request.body.user_id;
+    const { user_id } = request.body;
 
-    await db.query(
+    const result = await db.query(
         `INSERT INTO potholes (
             latitude,
             longitude,
@@ -38,17 +38,7 @@ export async function addPothole(request: Request, response: Response) {
 
     return response
         .status(200)
-        .json({
-            pothole_details: {
-                latitude,
-                longitude,
-                severity_level,
-                status,
-                direction,
-                image_link,
-                uploaded_by: user_id
-            }
-        });
+        .json({ pothole_details: result.rows[0] });
 }
 
 export async function getNearbyPotholes(request: Request, response: Response) {
@@ -74,5 +64,73 @@ export async function getNearbyPotholes(request: Request, response: Response) {
         [lat, lng, radius]
     );
 
-    response.json(result.rows.length);
+    return response.json(result.rows);
+}
+
+export async function updatePothole(request: Request, response: Response) {
+    const { id } = request.params;
+    const { pothole_details } = request.body;
+
+    const allowedDetailFields = [
+        "severity_level",
+        "direction",
+        "image_link"
+    ];
+
+    const updatableFields = Object
+        .keys(pothole_details)
+        .filter((field) => allowedDetailFields.includes(field));
+
+    if (updatableFields.length === 0) {
+        return response
+            .status(400)
+            .json({ message: "no valid data provided for pothole detail update" });
+    }
+
+    const setClause = updatableFields
+        .map((field, idx) => `${field} = $${idx + 1}`)
+        .join(", ");
+
+    const values = updatableFields.map((field) => pothole_details[field]);
+    values.push(id);
+
+    const query = `
+            UPDATE potholes
+            SET
+                ${setClause},
+                updated_at = NOW()
+            WHERE id = $${values.length}
+            RETURNING *
+        `;
+
+    const result = await db.query(query, values);
+
+    if (result.rows.length === 0) {
+        return response
+            .status(404)
+            .json({ message: "pothole not found" });
+    }
+
+    return response
+        .status(200)
+        .json(result.rows[0]);
+}
+
+export async function removePothole(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const result = await db.query(
+        `DELETE from potholes
+        WHERE id = $1
+        `,
+        [id]
+    );
+
+    if (result.rowCount === 0) {
+        return response
+            .status(404)
+            .json({ message: "pothole not found" });
+    }
+
+    return response.status(200).end();
 }
--- backend/src/routes/pothole.route.ts
+++ backend/src/routes/pothole.route.ts
@@ -1,19 +1,29 @@
 import { Router } from "express";
 import { tryCatchWrapper } from "../utils/try-catch.util.js";
 import { authenticateUser } from "../middlewares/auth.middleware.js";
-import { addPothole, getNearbyPotholes } from "../controllers/pothole.controller.js";
+import { addPothole, getNearbyPotholes, removePothole, updatePothole } from "../controllers/pothole.controller.js";
 
 
 const router = Router();
 
-router.route('/').post(
-    tryCatchWrapper(authenticateUser),
-    tryCatchWrapper(addPothole)
-);
+router.route('/')
+    .get(
+        tryCatchWrapper(authenticateUser),
+        tryCatchWrapper(getNearbyPotholes)
+    )
+    .post(
+        tryCatchWrapper(authenticateUser),
+        tryCatchWrapper(addPothole)
+    );
 
-router.route('/nearby').get(
-    tryCatchWrapper(authenticateUser),
-    tryCatchWrapper(getNearbyPotholes)
-);
+router.route('/:id')
+    .put(
+        tryCatchWrapper(authenticateUser),
+        tryCatchWrapper(updatePothole)
+    )
+    .delete(
+        tryCatchWrapper(authenticateUser),
+        tryCatchWrapper(removePothole)
+    );
 
 export default router;
